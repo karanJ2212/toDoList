bcf95b7de9b5b9d9bd588f81b59b068ec6c1c524
karan jain <karanjain2212@gmail.com>
2022-12-07 07:47:38 +0530

    initialize

---
+++ src/index.js
@@ -0,0 +1,35 @@
+class UI {
+  static displayTask() {
+    const task = [
+      {
+        desc: "buy fruits",
+        id: 1,
+      },
+      {
+        desc: "buy medicines",
+        id: 1,
+      },
+      {
+        desc: "buy household",
+        id: 1,
+      },
+    ];
+
+    task.forEach((works) => UI.addTasks(works));
+  }
+
+  static addTasks(works) {
+    const list = document.querySelector(".list");
+    let html = ` <li class="listItems">
+    <input type="checkbox" name="select" id="" />
+    <p class="title">${works.desc}</p>
+    <button class="button remove" id="${works.id}">Remove</button>
+  </li>`;
+    list.insertAdjacentHTML("beforeend", html);
+  }
+}
+
+//events
+document.getElementById("Enter").addEventListener("click", () => {
+  UI.displayTask();
+});
