6d48fe638898c8d75257f43772e8f8e0be24828c
karan jain <karanjain2212@gmail.com>
2022-12-07 08:07:55 +0530

    add linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
+// eslint-disable-next-line no-undef, no-unused-vars
 const path = require("path");
+// eslint-disable-next-line no-undef, no-unused-vars
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+// eslint-disable-next-line no-undef, no-unused-vars
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      template: "./src/index.html",
     }),
   ],
   output: {
     filename: "main.js",
+    // eslint-disable-next-line no-undef, no-unused-vars
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
